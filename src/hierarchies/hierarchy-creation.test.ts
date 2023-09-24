0bd49250d324c4d1c7b2ad60a6b4d21765b75c0f
Gaute Løken <gautelo@gmail.com>
2023-09-24 21:27:00 +0200

    Added tests for Hierarchies.createWithItems from items with parents or children.

--- src/hierarchies/hierarchy-creation.test.ts
+++ src/hierarchies/hierarchy-creation.test.ts
@@ -1,6 +1,7 @@
 import { MultiMap } from '@loken/utilities';
 import { expect, test } from 'vitest';
 
+import { traverseGraph } from '../traversal/traverse-graph.js';
 import { type Relation } from '../utilities/relations.js';
 import { Hierarchies } from './hierarchies.js';
 
@@ -48,6 +49,20 @@ test('Hierarchies.createWithIds(childMap)', () => {
 	expect(actual).toEqual(childMap);
 });
 
+test('Hierarchies.createWithIds(otherHierarchy)', () => {
+	const itemHc = Hierarchies.createWithItems({
+		items,
+		identify: item => item.id,
+		spec:     childMap,
+	});
+
+	const idHc = Hierarchies.createWithIds(itemHc);
+
+	const actual = idHc.toRelations();
+
+	expect(actual).toEqual(relations);
+});
+
 test('Hierarchies.createWithItems() from items and relations', () => {
 	const hc = Hierarchies.createWithItems({
 		items,
@@ -72,16 +87,76 @@ test('Hierarchies.createWithItems() from items and a child-map', () => {
 	expect(actual).toEqual(relations);
 });
 
-test('Hierarchies.createWithIds() from item hierarchy', () => {
-	const itemHc = Hierarchies.createWithItems({
-		items,
-		identify: item => item.id,
-		spec:     childMap,
+test('Hierarchies.createWithItems() from items with children', () => {
+	type ItemWithChildren = {
+		id: string,
+		children?: ItemWithChildren[],
+	}
+
+	// Represents the same structure as the `relations`
+	const itemsWithChildren: ItemWithChildren[] = [
+		{
+			id:       'A',
+			children: [
+				{ id: 'A1' },
+				{ id: 'A2', children: [ { id: 'A21' } ] },
+			],
+		}, {
+			id:       'B',
+			children: [
+				{
+					id:       'B1',
+					children: [
+						{ id: 'B11' },
+						{ id: 'B12' },
+						{ id: 'B13' },
+					],
+				},
+			],
+		},
+	];
+
+	const items = traverseGraph({
+		roots: itemsWithChildren,
+		next:  item => item.children ?? [],
 	});
 
-	const idHc = Hierarchies.createWithIds(itemHc);
+	const hc = Hierarchies.createWithItems({
+		items,
+		identify: item => item.id,
+		children: item => item.children,
+	});
 
-	const actual = idHc.toRelations();
+	const actual = hc.toRelations();
+
+	expect(actual).toEqual(relations);
+});
+
+test('Hierarchies.createWithItems() from items with parents', () => {
+	type ItemWithParent = {
+		id: string,
+		parent?: ItemWithParent,
+	}
+
+	// Clone the items into a map.
+	const itemsWithParents = new Map<string, ItemWithParent>();
+	for (const item of items)
+		itemsWithParents.set(item.id, { id: item.id });
+
+	// Set the parent references.
+	for (const [ parentId, childId ] of relations) {
+		const parent = itemsWithParents.get(parentId)!;
+		const child = itemsWithParents.get(childId)!;
+		child.parent = parent;
+	}
+
+	const hc = Hierarchies.createWithItems({
+		items:    itemsWithParents.values(),
+		identify: item => item.id,
+		parent:   item => item.parent,
+	});
+
+	const actual = hc.toRelations();
 
 	expect(actual).toEqual(relations);
 });
